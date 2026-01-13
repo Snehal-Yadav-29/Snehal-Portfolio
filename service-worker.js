950051bedce9c1af314161fe748ce5898a3be6d9
Snehal-Yadav-29 <snehalyadav.work29@gmail.com>
2026-01-13 11:46:34 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = "snehal-portfolio-v1";
+
+const FILES_TO_CACHE = [
+  "INDEX.HTML",
+  "manifest.json",
+  "icon.png"
+  
+];
+
+// Install event – cache files
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate event – remove old caches
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch event – serve offline content
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
